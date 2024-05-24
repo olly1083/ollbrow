2de325665e589f3a80ba88a748f0748519e3b854
olly1083 <142718035+olly1083@users.noreply.github.com>
2024-05-24 19:44:50 +0100

    Add files via upload

---
+++ ollbrow.py
@@ -0,0 +1,61 @@
+import sys
+import os
+from PyQt5.QtWidgets import QApplication, QMainWindow, QToolBar, QAction, QLineEdit
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtCore import QUrl
+
+class Browser(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.browser = QWebEngineView()
+        
+        # Load the custom home page
+        home_url = QUrl.fromLocalFile(os.path.abspath("home.html"))
+        self.browser.setUrl(home_url)
+
+        self.setCentralWidget(self.browser)
+        self.showMaximized()
+
+        # Navigation bar
+        navbar = QToolBar()
+        self.addToolBar(navbar)
+
+        back_btn = QAction("Back", self)
+        back_btn.triggered.connect(self.browser.back)
+        navbar.addAction(back_btn)
+
+        forward_btn = QAction("Forward", self)
+        forward_btn.triggered.connect(self.browser.forward)
+        navbar.addAction(forward_btn)
+
+        reload_btn = QAction("Reload", self)
+        reload_btn.triggered.connect(self.browser.reload)
+        navbar.addAction(reload_btn)
+
+        home_btn = QAction("Home", self)
+        home_btn.triggered.connect(self.navigate_home)
+        navbar.addAction(home_btn)
+
+        self.url_bar = QLineEdit()
+        self.url_bar.returnPressed.connect(self.navigate_to_url)
+        navbar.addWidget(self.url_bar)
+
+        self.browser.urlChanged.connect(self.update_url)
+
+    def navigate_home(self):
+        # Navigate to the custom home page
+        home_url = QUrl.fromLocalFile(os.path.abspath("home.html"))
+        self.browser.setUrl(home_url)
+
+    def navigate_to_url(self):
+        url = self.url_bar.text()
+        self.browser.setUrl(QUrl(url))
+
+    def update_url(self, q):
+        self.url_bar.setText(q.toString())
+
+app = QApplication(sys.argv)
+QApplication.setApplicationName("OLLBROW")
+window = Browser()
+app.exec_()
